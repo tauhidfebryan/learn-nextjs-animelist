1d1d0cc9e1e5cb7053a73cda909e4dfbb7cde10e
Tauhid Febryan <tauhidsamp@gmail.com>
2026-05-29 17:06:01 +0700

    Custom Loading

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+    return (
+        <div className='flex justify-center items-center min-h-screen'>
+            <div className='loading'></div>
+        </div>
+    )
+}
+
+export default loading
